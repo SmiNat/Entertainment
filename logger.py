438c066b383794b08b016076b8575881db38a2a4
Natalia <smieja.natalia@gmail.com>
2024-03-22 12:23:37 +0100

    changes in logger settings

--- logger.py
+++ logger.py
@@ -1,38 +1,39 @@
 import logging
 
-def setup_app_logging():
+
+def setup_entertainment_logging():
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s - %(name)s - %(message)s",
-        datefmt="%d-%b-%y %H:%M:%S",
+        format="%(asctime)s - %(levelname)s - %(name)s - %(filename)s:%(lineno)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[
-            # logging.FileHandler("app.log"),
+            # logging.FileHandler("entertainment.log"),
             logging.StreamHandler()
         ],
     )
+
+
+def setup_app_logging():
+    db_logger = logging.getLogger("app")
+    db_logger.setLevel(logging.INFO)
+
 
 def setup_db_logging():
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        datefmt="%d-%b-%y %H:%M:%S",
-        handlers=[
-            # logging.FileHandler("db.log"),
-            logging.StreamHandler()
-        ],
-    )
+    db_logger = logging.getLogger("db")
+    db_logger.setLevel(logging.DEBUG)
+
 
 def setup_test_logging():
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        datefmt="%d-%b-%y %H:%M:%S",
-        handlers=[
-            logging.FileHandler("test.log"),
-            logging.StreamHandler()
-        ],
+    test_logger = logging.getLogger("tests")
+    file_formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s"
     )
+    file_handler = logging.FileHandler("tests.log")
+    file_handler.setFormatter(file_formatter)
+    test_logger.addHandler(file_handler)
 
+
+setup_entertainment_logging()
 setup_app_logging()
 setup_db_logging()
 setup_test_logging()
