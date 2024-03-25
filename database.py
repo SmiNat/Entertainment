5d91eb8254ffaaa9711fb79b710c80fe8894a40b
Natalia <smieja.natalia@gmail.com>
2024-03-25 11:09:09 +0100

    changes in database.py - 2 new functions for creating db - for future use in tests if needed

--- database.py
+++ database.py
@@ -6,27 +6,43 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from logger import db_logger
 
 
-# Creating database with data from KAGGLE.COM
+# Note: data from KAGGLE.COM
 
-db = "entertainment.db"
-if os.path.exists(db):
-    db_logger.info("➡️  Database already exists.")
-else:
-    db_logger.info("➡️  Creating a database... (can take a while)")
-    import csv_converter
 
-    if os.path.exists(db):
-        db_logger.info("✅ Database 'entertainment' was successfully created.")
+def create_db(db_path: str):
+    if os.path.exists(db_path):
+        db_logger.info("➡️  Database already exists.")
+    else:
+        db_logger.info("➡️  Creating a database... (can take a while)")
+        import csv_converter
+
+        if os.path.exists(db_path):
+            db_logger.info("✅ Database 'entertainment' was successfully created.")
+
+
+def create_sqlite_engine(
+    db_path: str,
+    check_same_thread: bool = False,
+    echo: bool = False,
+    **kwargs
+):
+    return create_engine(
+        db_path,
+        connect_args={"check_same_thread": check_same_thread},
+        echo=echo,
+        **kwargs
+    )
+
 
 # Setting database for the project
 
+db = "entertainment.db"
+
+create_db(db)
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./entertainment.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    # echo=True  # for detail info about SQL commends
-)
+engine = create_sqlite_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
